3fe7346c6c922d6914a0efb88e02433030e3e47f
hanjunn22 <hanjunn22@gmail.com>
2024-02-08 00:19:09 +0900

    app.tsx와 main.tsx 작성

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { useState, useEffect } from 'react';
+import { fetchBreeds } from './api/dogs';
+import BreedList from './components/BreedList';
+import BreedImages from './components/BreedImages';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const Main = () => {
+  const [breed, setBreed] = useState<string>('');
+
+  useEffect(() => {
+    const getBreeds = async () => {
+      const data = await fetchBreeds();
+      console.log(data); // 데이터 확인 및 초기 품종 설정 로직 추가
+    };
+    
+    getBreeds();
+  }, []);
+
+  return (
+    <div>
+      <BreedList onSelectBreed={setBreed} />
+      {breed && <BreedImages breed={breed} />}
+    </div>
+  );
+}
+
+export default Main;
