79470ac77511b2dc865c4c8b5ca8cb51235f3a79
hanjunn22 <hanjunn22@gmail.com>
2024-02-08 00:47:53 +0900

    dogs.ts any대신 정확한 타입 명시

--- src/api/dogs.ts
+++ src/api/dogs.ts
@@ -1,19 +1,35 @@
-// src/api/dogs.ts
 import axios from 'axios';
 
 const API_BASE_URL = 'https://dog.ceo/api';
 
-export const fetchBreeds = async (): Promise<any> => {
-  const response = await axios.get(`${API_BASE_URL}/breeds/list/all`);
-  return response.data;
-};
-
-export const fetchBreedImages = async (breed: string): Promise<any> => {
-  const response = await axios.get(`${API_BASE_URL}/breed/${breed}/images`);
-  return response.data;
-};
-
-export const fetchRandomImages = async (count: number): Promise<any> => {
-    const response = await axios.get(`https://dog.ceo/api/breeds/image/random/${count}`);
-    return response.data;
+interface BreedResponse {
+  message: {
+    [key: string]: string[];
   };
+  status: string;
+}
+
+interface ImagesResponse {
+  message: string[];
+  status: string;
+}
+
+interface RandomImagesResponse {
+  message: string[];
+  status: string;
+}
+
+export const fetchBreeds = async (): Promise<BreedResponse> => {
+  const response = await axios.get<BreedResponse>(`${API_BASE_URL}/breeds/list/all`);
+  return response.data;
+};
+
+export const fetchBreedImages = async (breed: string): Promise<ImagesResponse> => {
+  const response = await axios.get<ImagesResponse>(`${API_BASE_URL}/breed/${breed}/images`);
+  return response.data;
+};
+
+export const fetchRandomImages = async (count: number): Promise<RandomImagesResponse> => {
+  const response = await axios.get<RandomImagesResponse>(`${API_BASE_URL}/breeds/image/random/${count}`);
+  return response.data;
+};
